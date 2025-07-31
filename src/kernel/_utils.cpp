030dfcc6ec6f8a8f9f2c3a3c98e5bc3e755d4261
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2025-07-31 13:37:05 +0200

    newasm

--- src/kernel/_utils.cpp
+++ src/kernel/_utils.cpp
@@ -26,7 +26,7 @@ namespace newasm
         {
             void krnl(std::string text)
             {
-                std::cout << newasm::header::col::gray << newasm::header::system_info::name << " KERNEL >> `"
+                std::cout << "\033[38;2;85;107;47m" << newasm::header::system_info::name << " KERNEL >> `"
                 << newasm::kernel::syscalls.at({newasm::threads::functions::get_sysenter(),newasm::mem::regs::fdx.get_value()})
                 <<"` | " << newasm::header::col::gray << text << newasm::header::col::reset << std::endl;
             }
