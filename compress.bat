afc01b9c5119599c94c710fb8f434f1a2bed220b
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2025-08-19 11:32:10 +0200

    newasm

--- compress.bat
+++ compress.bat
@@ -1,5 +1,5 @@
-del "extension.zip"
-del "extension.vsix"
+del "theme.zip"
+del "theme.vsix"
 powershell -Command "Compress-Archive -Path 'theme\*' -DestinationPath 'theme.zip' -Force"
 ren "theme.zip" "theme.vsix"
 echo Compressed...
