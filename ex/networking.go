04d86284840ca4f443941c3496001199fe90457e
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2025-08-21 19:31:14 +0200

    newasm

---
+++ ex/networking.go
@@ -0,0 +1,74 @@
+/*
+
+Version: MPL 1.1
+
+The contents of this file are subject to the Mozilla Public License Version 
+1.1 the "License"; you may not use this file except in compliance with 
+the License. You may obtain a copy of the License at 
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the
+License.
+
+Portions created by the Initial Developer are Copyright (c) The COPYRIGHT YEAR
+the Initial Developer. All Rights Reserved.
+
+*/
+
+package main
+
+import "C"
+import (
+    "fmt"
+    "net"
+    "unsafe"
+)
+
+//export send_tcp
+func send_tcp(addr *C.char, msg *C.char) C.int {
+    goAddr := C.GoString(addr)
+    goMsg := C.GoString(msg)
+
+    conn, err := net.Dial("tcp", goAddr)
+    if err != nil {
+        fmt.Println("NewASM GO :: Connection error -> ", err)
+        return -1
+    }
+    defer conn.Close()
+
+    _, err = conn.Write([]byte(goMsg))
+    if err != nil {
+        fmt.Println("NewASM GO :: Write error -> ", err)
+        return -2
+    }
+
+    return 0
+}
+
+//export recv_tcp
+func recv_tcp(addr *C.char) *C.char {
+    goAddr := C.GoString(addr)
+
+    conn, err := net.Dial("tcp", goAddr)
+    if err != nil {
+        return C.CString(fmt.Sprintf("NewASM GO :: Error -> %v", err))
+    }
+    defer conn.Close()
+
+    buf := make([]byte, 1024)
+    n, err := conn.Read(buf)
+    if err != nil {
+        return C.CString(fmt.Sprintf("NewASM GO :: Read error -> %v", err))
+    }
+
+    return C.CString(string(buf[:n]))
+}
+
+//export
+func newasm_networking_init() {
+	fmt.Println("NewASM GO :: Successfully loaded the netwoking system.")
+} 
+
+func main() {}
