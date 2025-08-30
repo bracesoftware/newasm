1dd410f2dee4500555c7b77f4f215e2eb63b77a0
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2025-08-30 12:02:46 +0200

    newasm

--- src/main.java
+++ src/main.java
@@ -17,10 +17,11 @@ the Initial Developer. All Rights Reserved.
 
 */
 
-public class NewASM_Graphics
+public class main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World.");
+        System.out.println("NewASM Graphics module loaded.");
+        return;
     }
 }
