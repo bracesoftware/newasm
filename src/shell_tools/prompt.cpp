0ec6af92b1be5bae19bf795f201e8105a32634d7
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2025-09-21 11:17:38 +0200

    x

---
+++ src/shell_tools/prompt.cpp
@@ -0,0 +1,37 @@
+/*
+
+Version: MPL 1.1
+
+The contents of this file are subject to the Mozilla Public License Version 
+1.1 the "License"; you may not use this file except in compliance with 
+the License. You may obtain a copy of the License at 
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the
+License.
+
+Portions created by the Initial Developer are Copyright (c) The COPYRIGHT YEAR
+the Initial Developer. All Rights Reserved.
+
+*/
+
+namespace newasm
+{
+    namespace shell_tools
+    {
+        inline bool prompt(std::string text)
+        {
+            std::string input;
+            std::cout << newasm::header::col::light_blue << ":: " << newasm::header::col::gray << text << newasm::header::col::light_blue << " [y/n] ";
+            std::cout << newasm::header::col::reset;
+            std::getline(std::cin, input);
+            std::cout << std::endl;
+
+            if((input == "Y") or (input == "y")) return true;
+            newasm::header::functions::err("Operation cancelled.");
+            return false;
+        }
+    }
+}
