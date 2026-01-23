8efd9bc974d54ba74137dad5a5ca7317cdea2cba
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2026-01-23 16:59:10 +0100

    b12

--- src/interpreter_newasm.cpp
+++ src/interpreter_newasm.cpp
@@ -90,7 +90,7 @@ the Initial Developer. All Rights Reserved.
 namespace newasm
 {
     //newasm::_std::shared_memory sharedMem;
-    const int BUILD_NUMBER = 11;
+    const int BUILD_NUMBER = 12;
     bool vercheck = true;
     bool dwin = false;
     const std::string tab = "\t\t\t";
