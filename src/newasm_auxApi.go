f50e0eb6e0b151a4e558f75b67151fe06455a999
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2026-01-24 15:33:51 +0100

    x

--- src/newasm_auxApi.go
+++ src/newasm_auxApi.go
@@ -24,12 +24,22 @@ package main
 import "C"
 
 import (
+    //for networking
     "fmt"
     "net"
     "net/http"
     "io"
     "strings"
     "unsafe"
+    //for console
+    "image/color"
+	"sync"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 //export send_tcp
@@ -125,4 +135,78 @@ func http_post(url *C.char, data *C.char) *C.char {
     }
 
     return C.CString(string(body))
+}
+
+/*
+CONSOLE FUNCTIONS
+*/
+
+var (
+	a         fyne.App
+	w         fyne.Window
+	logText   *canvas.Text
+	input     *widget.Entry
+	inputChan chan string
+	once      sync.Once
+)
+
+//export show_console
+func show_console(title *C.char) {
+	once.Do(func() {
+		inputChan = make(chan string)
+
+		a = app.New()
+		w = a.NewWindow(C.GoString(title))
+
+		logText = canvas.NewText("", color.RGBA{255, 255, 255, 255})
+		logText.TextSize = 14
+		logText.Alignment = fyne.TextAlignLeading
+
+		input = widget.NewEntry()
+		input.SetPlaceHolder("")
+
+		input.OnSubmitted = func(s string) {
+			logText.Text += s + "\n"
+			logText.Refresh()
+			inputChan <- s
+			input.SetText("")
+		}
+
+		bg := canvas.NewRectangle(color.RGBA{0, 0, 0, 255})
+
+		content := container.NewBorder(
+			nil,
+			input,
+			nil,
+			nil,
+			container.NewVScroll(logText),
+		)
+
+		w.SetContent(container.NewMax(bg, content))
+		w.Resize(fyne.NewSize(700, 450))
+
+		go w.ShowAndRun()
+	})
+}
+
+//export console_out
+func console_out(text *C.char) {
+	if logText == nil {
+		return
+	}
+	logText.Text += C.GoString(text)
+	logText.Refresh()
+}
+
+//export console_in
+func console_in() *C.char {
+	s := <-inputChan
+	return C.CString(s)
+}
+
+//export close_console
+func close_console() {
+	if w != nil {
+		w.Close()
+	}
 }
