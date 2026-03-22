906145c1069f382d8681b1beb77a72a44b79dca8
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2026-03-22 19:32:49 +0100

    more docs.

--- src/sys_boot__.cpp
+++ src/sys_boot__.cpp
@@ -33,16 +33,19 @@ namespace newasm::bootloader
 {
     int __main__(int argc, char* argv[])
     {
-        auto& dsk = NewASM::hardware::Disk;
-        NewASM::Drivers::FileSystem_V::DEFRAG(dsk);
-        if(!NewASM::Drivers::FileSystem_V::EXISTS(dsk, "hi.txt"))
+        if(false)
         {
-            NewASM::Drivers::FileSystem_V::MKFILE(dsk, "hi.txt", "Lmao");
+            auto& dsk = NewASM::hardware::Disk;
+            NewASM::Drivers::FileSystem_V::DEFRAG(dsk);
+            if(!NewASM::Drivers::FileSystem_V::EXISTS(dsk, "hi.txt"))
+            {
+                NewASM::Drivers::FileSystem_V::MKFILE(dsk, "hi.txt", "Lmao");
+            }
+            NewASM::Drivers::FileSystem_V::APPTOFILE(dsk, "hi.txt", "Hello!\n");
+            auto content = NewASM::Drivers::FileSystem_V::READFILE(dsk, "hi.txt");
+            std::cout << content;
         }
-        NewASM::Drivers::FileSystem_V::APPTOFILE(dsk, "hi.txt", "Hello!\n");
-        auto content = NewASM::Drivers::FileSystem_V::READFILE(dsk, "hi.txt");
-        std::cout << content;
-
+        
         newasm::sealedLabels = &newasm::compiler::data::sealed_labels;
         $defer //BC++ feature, $ blocks
             newasm::GLOBAL::cleanup();
