41ce6eb66d96c6ba2ee538b2216dcc4b86a75f07
Tractus Corticospinalis <zukanoviceltarik@gmail.com>
2026-05-01 10:42:51 +0200

    removed redundant code

--- src/libs._platformSpecific.cpp
+++ src/libs._platformSpecific.cpp
@@ -1,18 +1,6 @@
 // Copyright (c) 2026 Brace Software Co.
 // NewASM Virtual Machine and Toolchain
 
-#if _NEWASM_OS == _NEWASM_OS_windows
-    link "_platformSpecific/win32/libs";
-#elif _NEWASM_OS == _NEWASM_OS_linux
-    link "_platformSpecific/linux/libs";
-#elif _NEWASM_OS == _NEWASM_OS_windows_old
-    link "_platformSpecific/winold/libs";
-#elif _NEWASM_OS == _NEWASM_OS_android
-    link "_platformSpecific/android/libs";
-#elif _NEWASM_OS == _NEWASM_OS_macos
-    link "_platformSpecific/macos/libs";
-#endif
-
 module(cxx_int, {
     //setup
 });
